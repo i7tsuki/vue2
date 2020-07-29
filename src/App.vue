c9509673817e4aa1916058087b36717ecbe245f5
i7tsuki <tanaka_yi7@yahoo.co.jp>
2020-07-29 20:29:37 +0900

    進捗コミット200729（未完成）

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg=""/>
   </div>
 </template>
 
@@ -14,6 +13,7 @@ export default {
     HelloWorld
   }
 }
+
 </script>
 
 <style>
